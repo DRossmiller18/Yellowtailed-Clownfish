aca36faee876cc5b526ae7959eef1e96b69ec0fa
Dan Rossmiller <DanRossmiller@Michaels-iMac.fios-router.home>
2020-11-23 11:02:19 -0500

    This is the tutorial, but the data set did not load.

--- Hypotheses.R
+++ Hypotheses.R
@@ -1,3 +1,28 @@
-The population with the most genetic diversity would be in the Philippines. This is due to the fact that it is the amphiprion clarkii’s natural habitat.
-Indonesia has the second largest genetic diversity due to its proximity to the Philippines, even though a current does not travel down to Indonesia, and the equator.
-Japan has the least amount of genetic diversity because of its distance from the core populations. This large distance makes genetic flow difficult because Amphiprion clarkii do not travel very far within their lifetime.
+install.packages("tidyverse") # these packages were the ones you said I needed, but they aren't necessarily used here
+install.packages("hierfstat")
+install.packages("adegenet")
+install.packages("pegas")
+install.packages("Relatedness")
+mydata <= read.table("Master_Pinus_data_genotype.txt", header = TRUE)
+dim(mydata) 
+locus <- mydata[ -c(1, 2, 3, 4, 17:3086)]
+colnames(locus) <- gsub("\\.", "_", colnames(locus))
+ind <- as.character(mydata$tree_id)
+population <- as.character(mydata$state)
+mydata1 <- df2genind(locus, ploidy = 2, ind.names = ind, pop = population, sep = " ")
+mydata1
+nA11(mydata1)
+div <- summary(mydata1)
+div
+names(div)
+plot(div$Hobs, xlab = "Loci number", ylab = "Observed Heterogygosity", main = "Observed heterozygosity per locus")
+plot(div$Hobs, div$Hexp, xlab = "Hobs", ylab = "Hexp", main = "Expectered heterozygosity as a function of observed heterozygosity per locus")
+bartlett.test(list(div$Hexp, div$Hobs))
+basicstat <- basic.stats(mydata2, diploid = TRUE, digits = 2)
+names(basicstat)
+boot.ppfis(mydata2)
+x <- indpca (mydata2)
+plot(x, cex = 0.7)
+hw.test(mydata1, B = 1000)
+options(width = 100)
+devtools::session_info() # The data did not properly download, but seeing the tutorial I should've had graphs 
